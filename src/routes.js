4d7edae65968d06bc29eb5aa45d4403300b7f388
Igor Santos <igor.sagitarius@gmail.com>
2020-12-03 09:39:40 -0300

    Exclusão da pagina Fake trailers

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import {BrowserRouter,Route} from 'react-router-dom'
 import Landing from './pages/landing'
 import PlanetTerror from './pages/planetTerror.js'
 import DeathProof from './pages/deathProof.js'
-import FakeTrailers from './pages/fakeTrailers.js'
 import About from './pages/about.js'
 
 function Routes(){
@@ -13,7 +12,6 @@ function Routes(){
       <Route path="/about" component={About} />
       <Route path="/planetTerror" component={PlanetTerror} />
       <Route path="/deathProof" component={DeathProof} />
-      <Route path="/fakeTrailers" component={FakeTrailers} />
     </BrowserRouter>
   )
 }
