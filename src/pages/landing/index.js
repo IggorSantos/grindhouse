93b94b84be0a5bc4bf253d88689aaa65d81535a9
Igor Santos <igor.sagitarius@gmail.com>
2021-11-04 14:38:06 -0300

    Modificação do estilo da landing-page para styled components

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Bg from '../../assets/images/Grindhouse_banner.jpg'
 import Menu from '../../components/menu/index'
-import './styles.css'
+import styled from 'styled-components'
 
 const Landing = () => {
   return(
-    <div id="page-landing">
-    <Menu />
-    <img src={Bg} alt="Background" className="img" />
+    <div>
+      <Menu />
+      <Img src={Bg} alt="Background" className="img" />
     </div>
   )
 }
 
 export default Landing;
+
+const Img = styled.img `
+  width: 100%;
+  height: 100%;
+  padding-top: 2%;
+  `
