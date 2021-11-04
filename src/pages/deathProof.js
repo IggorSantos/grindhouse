0c798d919a29c96e827957fe30db716dc496ec7d
Igor Santos <igor.sagitarius@gmail.com>
2021-11-04 15:24:34 -0300

    Conclusão da componentização de todas paginas e utilizando styled-components

--- src/pages/deathProof.js
+++ src/pages/deathProof.js
@@ -2,24 +2,24 @@ import React from 'react'
 import Menu from '../components/menu'
 import BgDeathProof from '../assets/images/death.jpg'
 import PageHeader from '../components/pageHeader'
+import ContentText from '../components/content-text'
 import styled from 'styled-components'
 
 const DeathProof = () => {
   return (
-    <div id="page">
-    <Menu />
-    <PageHeader name="A Prova de Morte" small="de Quentin Tarantino" />
-    <div className="imgPoster">
-     <img src={BgDeathProof} alt="Poster A prova de Morte"/>
-    </div>
-    <div className="container-texto">
-     <p>O dublê Mike (Kurt Russell) é um profissional que gosta de levar mulheres
-     inocentes para passeios de carro mortais em suas horas vagas. Ele adaptou seu
-     carro para impactos máximos. Enquanto provoca acidentes e as vítimas se acumulam,
-     ele se safa sem praticamente nenhum arranhão. Mas ele não esperava pelo grupo de
-     mulheres duronas que inclui Zoe Bell, dublê na vida real.</p>
-    </div>
-
+    <div>
+      <Menu />
+      <PageHeader name="A Prova de Morte" small="de Quentin Tarantino" />
+      <DeathContent>
+         <ImgPoster src={BgDeathProof} alt="Poster A prova de Morte"/>
+         <ContentText>
+            O dublê Mike (Kurt Russell) é um profissional que gosta de levar mulheres
+            inocentes para passeios de carro mortais em suas horas vagas. Ele adaptou seu
+            carro para impactos máximos. Enquanto provoca acidentes e as vítimas se acumulam,
+            ele se safa sem praticamente nenhum arranhão. Mas ele não esperava pelo grupo de
+            mulheres duronas que inclui Zoe Bell, dublê na vida real.
+        </ContentText>
+      </DeathContent>
   </div>
   )
 }
--- src/pages/planetTerror.js
+++ src/pages/planetTerror.js
@@ -2,24 +2,36 @@ import React from 'react'
 import Menu from '../components/menu'
 import BgPlanetTerror from '../assets/images/terror-wallpaper.jpg'
 import PageHeader from '../components/pageHeader'
-import './styles.css'
+import ContentText from '../components/content-text'
+import styled from 'styled-components'
+
 
 const PlanetTerror = () => {
   return (
-    <div id="page">
-    <Menu />
-    <PageHeader name="Planeta Terror" small="de Robert Rodriguez" />
-    <div className="imgPoster">
-     <img src={BgPlanetTerror} alt="Poster Grindhouse" />
-    </div>
-    <div className="container-texto">
-     <p>Uma experiência fracassada libera vírus que transforma humanos em zumbis
-     sedentos de sangue e carnívoros. El Wray, Cherry, um médico e o xerife unem
-     forças para sobreviver à noite, quando os mutantes ameaçam tomar a cidade e
-     o mundo.</p>
-    </div>
+    <div>
+      <Menu />
+      <PageHeader name="Planeta Terror" small="de Robert Rodriguez" />
+      <PlanetContent>
+         <ImgPoster src={BgPlanetTerror} alt="Poster Grindhouse" />
+         <ContentText>
+            Uma experiência fracassada libera vírus que transforma humanos em zumbis
+            sedentos de sangue e carnívoros. El Wray, Cherry, um médico e o xerife unem
+            forças para sobreviver à noite, quando os mutantes ameaçam tomar a cidade e
+            o mundo.
+         </ContentText>
+      </PlanetContent>
   </div>
   )
 }
 
 export default PlanetTerror;
+
+const PlanetContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  `
+const ImgPoster = styled.img`
+  width:600px;
+  height: 400px;
+`
